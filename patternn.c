13d8b48e80239515b2bc9726d234e320e97cf307
Bhimani Maulik <140812702+gituhihi@users.noreply.github.com>
2023-10-01 20:32:01 +0530

    new

--- patternn.c
+++ patternn.c
@@ -1,15 +1,23 @@
 #include<stdio.h>
 
 int main(){
-    int i,j,n=11;
-    for(i=1;i<=n;i++){
-        if(i%3==0||i==1){
+int i,j,n=11;
+    for(i=1;i<=n;i++)
+    {
+        if(i%3==0||i==1)
+        {
             printf("%d", i);
-        }else{
-            for(j=1;j<=i;j++){
-                if(j%2==0){
+        }
+        else
+        {
+            for(j=1;j<=i;j++)
+            {
+                if(j%2==0)
+                {
                     printf("*");
-                }else{
+                }
+                else
+                {
                     printf("%d",j);
                 }
             }
