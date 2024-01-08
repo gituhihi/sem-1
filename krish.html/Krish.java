6dd6581e59e61b099c40de4c597cb9c85a72afd5
Bhimani Maulik <140812702+gituhihi@users.noreply.github.com>
2024-01-08 09:03:20 +0530

    Add files via upload

---
+++ krish.html/Krish.java
@@ -0,0 +1,7 @@
+public class krish
+{
+	 public static void main(String[] args) {
+		
+		System.out.println("hello world.");
+	}
+}
---
+++ krish.html/Krish1.java
@@ -0,0 +1,7 @@
+public class Krish1
+{
+	 public static void main(String[] args) {
+		
+		System.out.println("hello world.");
+	}
+}
